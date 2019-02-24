197ec954a074dbd471c09a3a88f47848e5230bde
Junior Sanchez <r.a.s.g65@gmail.com>
2019-02-24 03:34:30 -0400

    add app users

--- platzigram/settings.py
+++ platzigram/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     #locals app
+    'users.apps.UsersConfig',
     'posts.apps.PostsConfig',
 ]
 
